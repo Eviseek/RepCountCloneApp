e7f82d7ac7da3112c79268413dadc6347a4a83cf
Eva Chlpikova <chlpikova.e@gmail.com>
2023-10-08 22:06:14 +0200

    Initial Commit

---
+++ RepCountCopy/RepCountCopyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepCountCopyApp.swift
+//  RepCountCopy
+//
+//  Created by Eva Chlpikova on 08.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct RepCountCopyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
